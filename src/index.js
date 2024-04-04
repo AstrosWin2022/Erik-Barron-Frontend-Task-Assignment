bd9ab70bccd8431702773f70958ae051a5616f09
AstrosWin2022 <112510106+AstrosWin2022@users.noreply.github.com>
2024-04-04 15:56:44 -0500

    Change variable name

--- src/index.js
+++ src/index.js
@@ -6,12 +6,12 @@ import { Provider } from 'react-redux'
 import { createStore } from 'redux';
 import reducer from './reducer/reducers';
 
-const initialState = {
+const defaultState = {
   posts: [],
   name: 'Erik Barron'
 };
 
-const _store = createStore(reducer, initialState);
+const _store = createStore(reducer, defaultState);
 
 ReactDOM.render(
   <React.StrictMode>
